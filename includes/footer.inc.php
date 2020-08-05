fcbfa0a3cb786dc2f1cf6e69acc5a02c50451136
Paul J Shaw <pavlvsxavier@gmail.com>
2020-08-05 15:05:23 +0100

    First commit

---
+++ includes/footer.inc.php
@@ -0,0 +1,9 @@
+</div>
+<!--End of topContainer -->
+
+</div>
+<!--End of mainContainer -->
+<!-- <script src="assets/js/script.js"></script> -->
+</body>
+
+</html>
---
+++ includes/header.inc.php
@@ -0,0 +1,22 @@
+<?php
+include "includes/config.inc.php";
+include "includes/autoloader.inc.php";
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Change me</title>
+    <link rel="stylesheet" href="assets/css/style.css">
+    <!-- <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script> -->
+    <script src="assets/js/jquery-3.4.1.js"></script>
+    <script src="assets/js/script.js"></script>
+</head>
+
+<body>
+    <div id="mainContainer">
+        <div id="topContainer">
